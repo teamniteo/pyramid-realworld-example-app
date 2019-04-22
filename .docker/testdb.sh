520c7a1a1bf1e5006110b4ce73e04b6614f62394
Arijit Basu <sayanarijit@gmail.com>
2019-04-22 15:54:10 +0530

    Fix pgsql connection error on circle ci
    - Added PGHOST: localhost
    - Used direct URL for DB instead of -h, --user etc.

--- .docker/testdb.sh
+++ .docker/testdb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-psql -h localhost -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql "postgresql://$POSTGRES_USER@:5432/$POSTGRES_DB" -v ON_ERROR_STOP=1 <<-EOSQL
   CREATE USER conduit_test WITH PASSWORD '';
   CREATE DATABASE conduit_test;
   GRANT ALL PRIVILEGES ON DATABASE conduit_test TO conduit_test;
 EOSQL
 
-psql -h localhost -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "conduit_test" <<-EOSQL
+psql -v ON_ERROR_STOP=1 "postgresql://$POSTGRES_USER@:5432/conduit_test" <<-EOSQL
   CREATE EXTENSION IF NOT EXISTS pgcrypto;
   SELECT gen_random_uuid();
 EOSQL
