d71ef520203a45aa2197aa375ede2c80c322886a
Nejc Zupan <nejc.zupan@gmail.com>
2020-03-15 21:53:14 +0000

    Cleanup
    Only need to run this once, not on every release.

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -3,10 +3,6 @@
 
 set -e
 
-# See https://github.com/niteoweb/pyramid-realworld-example-app/issues/86
-echo "Installing required extensions"
-psql $DATABASE_URL -c "CREATE EXTENSION IF NOT EXISTS pgcrypto;"
-
 echo "Running database migrations"
 alembic -c etc/alembic.ini -x ini=etc/production.ini upgrade head || echo "Database migrations failed!"
 echo "Done"
